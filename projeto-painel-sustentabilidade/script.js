b243ff6dd25beb5447abdb44d88d617aa557d854
Arthur Andrade Duarte <arthurandrade2006@gmail.com>
2025-06-16 11:47:25 -0300

    Update script.js

--- projeto-painel-sustentabilidade/script.js
+++ projeto-painel-sustentabilidade/script.js
@@ -12,7 +12,7 @@ window.onload = function() {
     criarGrafico('graficoLuz', 'luz');
     atualizarAlertas();
 };
-
+// Faz a criação do gráfico.
 function criarGrafico(canvasId, tipo) {
     const ctx = document.getElementById(canvasId).getContext('2d');
     graficos[tipo] = new Chart(ctx, {
@@ -34,31 +34,31 @@ function criarGrafico(canvasId, tipo) {
         }
     });
 }
-
+// Ajusta o gráfico e os dados.
 function ajustar(tipo, valor) {
     dados[tipo] = Math.max(0, Math.min(100, dados[tipo] + valor));
     atualizarGrafico(tipo);
     atualizarAlertas();
 }
-
+// Atualiza o gráfico.
 function atualizarGrafico(tipo) {
     graficos[tipo].data.datasets[0].data[0] = dados[tipo];
     graficos[tipo].data.datasets[0].backgroundColor = getCor(dados[tipo]);
     graficos[tipo].update();
 }
-
+// Adiciona cor ao grafico.
 function getCor(valor) {
     if (valor >= 70) return 'rgba(220,20,60,0.7)';       // Vermelho
     if (valor <= 30) return 'rgba(30,144,255,0.7)';      // Azul
     return 'rgba(50,205,50,0.7)';                        // Verde
 }
-
+//Atualiza os alertas de consumos.
 function atualizarAlertas() {
     document.getElementById('alertaEnergia').innerText = gerarMensagem('energia');
     document.getElementById('alertaAgua').innerText = gerarMensagem('agua');
     document.getElementById('alertaLuz').innerText = gerarMensagem('luz');
 }
-
+// Gera a mensagem de alerta.
 function gerarMensagem(tipo) {
     let valor = dados[tipo];
     if (valor >= 70) return `⚠️ Consumo de ${tipo} muito alto!`;
